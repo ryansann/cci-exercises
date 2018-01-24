87aca246496a82e77b893e1ab5f74450ce9a6bc7
Ryan Sanna <ryansann@umich.edu>
2018-01-24 16:05:06 -0700

    gitignore addition and main.cpp code

---
+++ ArrayList/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "ArrayList.h"
+
+using namespace std;
+
+int main() {
+
+  //ArrayList<int> myArrayList(1);
+
+  //myArrayList[0] = 1;
+
+  cout << "HELLO" << endl;
+
+  //myArrayList.print();
+
+  return 0;
+};
