4f9356b0f0915fabccb10abf66324270710b05ca
Ryan Sanna <ryansann@umich.edu>
2018-01-29 14:25:08 -0700

    minor adjustments and notes in 1.1

--- 1/1/index.js
+++ 1/1/index.js
@@ -1,9 +1,14 @@
+// This assumes the ASCII character set
+
 // with additional data structures
 // return true if string is comprised of only unique characters otherwise return false
-// O(n)
+// O(n) time
+// O(n) space
 function checkUnique(str) {
   if (typeof str !== "string") {
     return false;
+  } else if (str.length > 128) {
+    return false; // must be dupes if we have more than 128 characters 
   }
   charHash = {};
   for (char of str) {
@@ -21,10 +26,13 @@ function checkUnique(str) {
 
 // without additional data structures
 // return true if string is comprised of only unique characters otherwise return false
-// O(n)
+// O(n^2) time
+// O(1) space
 function checkUnique2(str) {
   if (typeof str !== "string") {
     return false;
+  } else if (str.length > 128) {
+    return false; // must be dupes if we have more than 128 characters 
   }
   for (i = 0; i < str.length; i++) {
     for(j = i + 1; j < str.length; j++) {
@@ -45,3 +53,10 @@ console.log(`Checking 'dub spc ': ${checkUnique("dub spc ")}`);
 console.log(`Checking 'about': ${checkUnique2("about")}`);
 console.log(`Checking 'bigger string': ${checkUnique2("bigger string")}`);
 console.log(`Checking 'dub spc ': ${checkUnique2("dub spc ")}`);
+
+// Note: Think about the character set during an interview question
+// Space and time complexity are dependent on the character set in this example
+
+// Did not consider:
+// - bit vectors
+// - sorting string and then comparing
