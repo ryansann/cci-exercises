c3381d9fc91303c1ff130886286e6c0646c928f1
Ryan Sanna <ryansann@umich.edu>
2018-01-30 13:28:29 -0700

    1.5

---
+++ 1.5/README.md
@@ -0,0 +1,5 @@
+There are three types of edits that can be performed:  
+- insert a character
+- remove a character
+- replace a character  
+Given 2 strings, write a function to check if they are one (or zero) edits away from one another.  
---
+++ 1.5/index.js
@@ -0,0 +1,50 @@
+// Check if strings are one edit away from one another
+// return true if they are one edit away, otherwise return false
+
+// O(n) time
+// O(n) space
+function checkEdits(str1, str2) {
+  // more than one character length difference implies > 1 edit away
+  if (Math.abs(str1.length - str2.length) > 1) return false;
+
+  // if more than one character is different then we are not one edit away
+  // build charMap
+  let charMap = {}
+  for (let c of str1) {
+    if (charMap[c]) {
+      charMap[c]++;
+    } else {
+      charMap[c] = 1;
+    }
+  }
+  
+  // track number of edits
+  let numEdits = 0;
+  
+  // update charMap with str2
+  for (let c of str2) {
+    if (charMap[c]) {
+      charMap[c]--;
+    }
+  }
+
+  // iterate charMap to count rest of edits
+  for (let c in charMap) {
+    numEdits += charMap[c];
+  }
+
+  if (numEdits > 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Tests
+console.log("Check true:");
+console.log(`checkEdits('pale', 'ple'): ${checkEdits('pale', 'ple')}`);
+console.log(`checkEdits('pales', 'pale'): ${checkEdits('pales', 'pale')}`);
+console.log(`checkEdits('pale', 'bale'): ${checkEdits('pale', 'bale')}`);
+console.log("Check false:");
+console.log(`checkEdits('pale', 'bake'): ${checkEdits('pale', 'bake')}`);
+console.log(`checkEdits('paleeee', 'bake'): ${checkEdits('paleeee', 'bake')}`);
