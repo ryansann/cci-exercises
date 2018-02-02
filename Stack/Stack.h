595d223615f60ad1a3172e71951b57b1deaa80ad
Ryan Sanna <ryansann@umich.edu>
2018-02-01 18:47:16 -0700

    Stack class

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -5,121 +5,91 @@
 
 // Included Dependencies
 #include <iostream>
-
-// Class Interface
-template <typename T>
-class ArrayList {
-  
-  private:
-    const static int initialCapacity = 2; // initial number of elements
-    const static int reallocationMultiplier = 2; // multiplier for reallocating array
-    int size; // this tracks numnber of elements in arrayList
-    int maxCapacity; // this tracks current max capacity of arrayList
-    T *arrayList;
-
-  public:
-    // instance ops
-    ArrayList();
-    ArrayList(const ArrayList<T> &original);
-    ~ArrayList();
-
-    // operator overloads
-    T &operator[](int index);
-
-    // getters
-    int getSize();
-
-    // array ops
-    void appendElement(T &element);
-    void deleteElement(unsigned int index);
-    void clear();
-    void print();
-
-};
+#include "StackNode.h"
 
 using namespace std;
 
+// Class Interface
+template <typename T>
+class Stack {
+  private:
+    int size;
+    StackNode<T> *top;
+    StackNode<T> *tmp;
+  public:
+    Stack();
+    ~Stack();
+    StackNode<T>& pop();
+    void push(StackNode<T> &n);
+    void push(T data);
+    StackNode<T>& peek();
+    bool isEmpty();
+    void clear();
+};
+
 // Class Definitions
 template <typename T>
-ArrayList<T>::ArrayList() {
+Stack<T>::Stack() {
   size = 0;
-  maxCapacity = initialCapacity;
-  arrayList = new T[maxCapacity];
+  top = 0;
 }
 
 template <typename T>
-ArrayList<T>::ArrayList(const ArrayList<T> &original) {
-  // this could be made more efficient by reducing number of resize opts
-  ArrayList<T> copy = ArrayList();
-  for(int i = 0; i < size; i++) {
-    copy.append(original[i]);
-  }
-  return copy;
-}
-
-template <typename T>
-ArrayList<T>::~ArrayList() {
+Stack<T>::~Stack() {
   clear();
-}
-
-template <typename T>
-T &ArrayList<T>::operator[](int index) {
-  return arrayList[index]; // return reference
-}
-
-template <typename T>
-int ArrayList<T>::getSize() {
-  return size;
-}
-
-// Resize logic is in this method
-template <typename T>
-void ArrayList<T>::appendElement(T &element) {
-  cout << "Appending element with address: " << &element << '\n';
-  if ((size + 1) > maxCapacity) { // we hit the upper bound on the number of T elements arrayList can hold
-    cout << "Resize Operation Triggered" << '\n';
-    maxCapacity *= reallocationMultiplier;
-    T *temp = new T[maxCapacity]; // allocate a bigger chunk of memory for arrayList
-    for (int i = 0; i < size; i++) {
-      temp[i] = arrayList[i]; // copy our values over O(n)
-    }
-    delete[] arrayList; // free old array memory
-    arrayList = temp; // set arrayList to larger memory array
-  }
-  arrayList[size] = element; // perform append operation
-  size++;
   return;
 }
 
 template <typename T>
-void ArrayList<T>::deleteElement(unsigned int index) {
-  if (index > (size - 1)) { // not in arrayList
-    return;
-  } else if (size == 1) { // arrayList is only one element, so clear it
-    clear();
-  } else { // shift elements past index over one to left
-    for (int i =  index; i < size; i++) {
-      arrayList[i] = arrayList[i + 1];
-    }
+StackNode<T>& Stack<T>::pop() {
+  if (top) {
+    tmp = top;
+    top = tmp->next;
     size--;
+    return tmp;
+  } else {
+    return 0;
   }
 }
 
 template <typename T>
-void ArrayList<T>::clear() {
-  if (arrayList) {
-    delete[] arrayList; // free memory allocated for arrayList
-    size = 0;
-    arrayList = new T[initialCapacity];
-    maxCapacity = initialCapacity;
+void Stack<T>::push(StackNode<T> &n) {
+  n->next = top;
+  top = n;
+  size++;
+}
+
+template <typename T>
+void Stack<T>::push(T data) {
+  StackNode<T> *n = new StackNode<T>();
+  n->data = data;
+  n->next = top;
+  top = n;
+  size++;
+}
+
+template <typename T>
+StackNode<T>& Stack<T>::peek() {
+  return top;
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() {
+  if (size == 0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
 template <typename T>
-void ArrayList<T>::print() {
-  cout << "Current arrayList size = " << size << '\n';
-  cout << "Current arrayList capacity = " << maxCapacity << '\n';
-  for(int i = 0; i < size; i++) {
-    cout << "ArrayList[" << i << "] = " << "&" << &arrayList[i] << '\n';
+void Stack<T>::clear() {
+  while(top) {
+    tmp = top->next;
+    delete top;
+    top = tmp;
   }
+  size = 0;
+  top = 0;
+  return;
 }
---
+++ Stack/main.cpp
@@ -0,0 +1,9 @@
+#include "Stack.h"
+#include "StackNode.h"
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
